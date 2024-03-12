1648dd49eb0e24a2863b43d3559d85292b40db78
Marcin Orlowski <marcin@orlowski.it>
2024-03-12 14:24:16 +0100

    WIP: H3m map reader - tests: read map type, basic map parameters

--- pkg/h3m/Reader_test.go
+++ pkg/h3m/Reader_test.go
@@ -41,3 +41,103 @@ func TestLoad(t *testing.T) {
 		})
 	}
 }
+
+func Test_loadHeader(t *testing.T) {
+	type args struct {
+		mapFileName string
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantErr        bool
+		expectedHeader string
+	}{
+		{
+			name:           "SoD_map",
+			args:           args{mapFileName: "test_maps/test_1.h3m"},
+			wantErr:        false,
+			expectedHeader: "SoD",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h3m, err := Load(tt.args.mapFileName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("loadHeader() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if h3m.Format != tt.expectedHeader {
+				t.Errorf("loadHeader() got = %v, want %v", h3m.Format, tt.expectedHeader)
+				return
+			}
+		})
+	}
+}
+
+func Test_loadBasicMapParameters(t *testing.T) {
+	type basicMapParameters struct {
+		HasHeroOnMap   bool
+		MapSize        uint32
+		HasUnderground bool
+		Name           string
+		Description    string
+	}
+
+	type args struct {
+		mapFileName string
+	}
+	tests := []struct {
+		name                string
+		args                args
+		wantErr             bool
+		expectedBasicParams basicMapParameters
+	}{
+		{
+			name:    "test_1.h3m",
+			args:    args{mapFileName: "test_maps/test_1.h3m"},
+			wantErr: false,
+			expectedBasicParams: basicMapParameters{
+				HasHeroOnMap:   true,
+				MapSize:        36,
+				HasUnderground: false,
+				Name:           "Test Map 1",
+				Description:    "Map Type SoD\nTest Map description 1\nDifficulty level - Low\nNo teams\nNo special wins\nNo special loose",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h3m, err := Load(tt.args.mapFileName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("loadBasicMapParameters() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if h3m.HasHeroOnMap != tt.expectedBasicParams.HasHeroOnMap {
+				t.Errorf("loadBasicMapParameters.HasHeroOnMap got = %v, want %v", h3m.HasHeroOnMap, tt.expectedBasicParams.HasHeroOnMap)
+				return
+			}
+
+			if h3m.MapSize != tt.expectedBasicParams.MapSize {
+				t.Errorf("loadBasicMapParameters.MapSize got = %v, want %v", h3m.MapSize, tt.expectedBasicParams.MapSize)
+				return
+			}
+
+			if h3m.HasUnderground != tt.expectedBasicParams.HasUnderground {
+				t.Errorf("loadBasicMapParameters.HasUnderground got = %v, want %v", h3m.HasUnderground, tt.expectedBasicParams.HasUnderground)
+				return
+			}
+
+			if h3m.Name != tt.expectedBasicParams.Name {
+				t.Errorf("loadBasicMapParameters.Name got = %v, want %v", h3m.Name, tt.expectedBasicParams.Name)
+				return
+			}
+
+			if h3m.Description != tt.expectedBasicParams.Description {
+				t.Errorf("loadBasicMapParameters.Description got = %v, want %v", h3m.Description, tt.expectedBasicParams.Description)
+				return
+			}
+		})
+	}
+}
