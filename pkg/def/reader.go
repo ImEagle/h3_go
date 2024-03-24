f91ebb62e88b3e8e850b7e425a1fd97cd8c25f75
Marcin Orlowski <marcin@orlowski.it>
2024-03-24 10:47:51 +0100

    WIP: Def image export - export file type 0

--- pkg/def/reader.go
+++ pkg/def/reader.go
@@ -4,7 +4,14 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	_ "image/png"
 	"io"
+	"os"
 )
 
 const SpriteSheetType = 0x42
@@ -81,6 +88,7 @@ func (r *Reader) fetchImages(bReader *bytes.Reader) error {
 
 		for _, offset := range r.blocks[i].Offsets {
 			var imgFrame frame
+			var pixelData []byte
 
 			bReader.Seek(int64(offset), io.SeekStart)
 			binary.Read(bReader, binary.LittleEndian, &imgFrame)
@@ -94,13 +102,56 @@ func (r *Reader) fetchImages(bReader *bytes.Reader) error {
 				firstFullWidth = int(imgFrame.FullWidth)
 				firstFullHeight = int(imgFrame.FullHeight)
 			} else {
-				// TODO
+				if firstFullWidth > int(imgFrame.FullWidth) {
+					imgFrame.FullWidth = uint32(firstFullWidth)
+				}
+
+				if firstFullWidth < int(imgFrame.FullWidth) {
+					firstFullWidth = int(imgFrame.FullWidth)
+				}
+
+				if firstFullHeight > int(imgFrame.FullHeight) {
+					imgFrame.FullHeight = uint32(firstFullHeight)
+				}
+
+				if firstFullHeight < int(imgFrame.FullHeight) {
+					firstFullHeight = int(imgFrame.FullHeight)
+					return errors.New("first height smaller than latter one")
+				}
+
+				if imgFrame.Width != 0 || imgFrame.Height != 0 {
+					if imgFrame.Format == 0 {
+						var err error
+						pixelData, err = extractFromFormat0(imgFrame, bReader)
+						if err != nil {
+							return err
+						}
+
+					}
+
+				}
+
+				err := debugCreateImageFromBytes(pixelData, r.palette, imgFrame, fmt.Sprintf("image_%d.png", i))
+				if err != nil {
+					return err
+				}
+
 			}
 
 		}
 	}
 
 	return nil
+}
+
+func extractFromFormat0(imgFrame frame, reader *bytes.Reader) ([]byte, error) {
+	pixelData := make([]byte, imgFrame.Width*imgFrame.Height)
+	err := binary.Read(reader, binary.LittleEndian, &pixelData)
+	if err != nil {
+		return nil, err
+	}
+
+	return pixelData, nil
 }
 
 func (r *Reader) CanGenerateSpriteSheet() bool {
@@ -170,4 +221,57 @@ func readString(f io.Reader, len int) (string, error) {
 	fileName := buf[:nullIndex]
 
 	return string(fileName), nil
+}
+
+func debugCreateImageFromBytes(data []byte, palette []byte, imgFrame frame, fileName string) error {
+
+	pal := make([]color.Color, len(palette)/3)
+	for i := 0; i < len(palette); i += 3 {
+		pal[i/3] = color.RGBA{palette[i], palette[i+1], palette[i+2], 0xff}
+	}
+
+	imp := image.NewPaletted(image.Rect(0, 0, int(imgFrame.FullWidth), int(imgFrame.FullHeight)), pal)
+	copy(imp.Pix, data)
+
+	imRGB := image.NewRGBA(imp.Bounds())
+	draw.Draw(imRGB, imRGB.Bounds(), imp, image.Point{}, draw.Src)
+
+	replaceColor(imRGB, color.RGBA{0, 0, 0, 0}, palette, 0)
+	replaceColor(imRGB, color.RGBA{0, 0, 0, 0x40}, palette, 1)
+	replaceColor(imRGB, color.RGBA{0, 0, 0, 0x80}, palette, 4)
+	replaceColor(imRGB, color.RGBA{0, 0, 0, 0}, palette, 5)
+	replaceColor(imRGB, color.RGBA{0, 0, 0, 0x80}, palette, 6)
+	replaceColor(imRGB, color.RGBA{0, 0, 0, 0x40}, palette, 7)
+
+	im := image.NewRGBA(image.Rect(0, 0, int(imgFrame.FullWidth), int(imgFrame.FullHeight)))
+	draw.Draw(im, im.Bounds(), &image.Uniform{color.RGBA{0, 0, 0, 0}}, image.Point{}, draw.Src)
+	draw.Draw(im, im.Bounds(), imRGB, image.Point{int(imgFrame.LeftMargin), int(imgFrame.TopMargin)}, draw.Src)
+
+	// Create or open the file for writing
+	file, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("error creating/opening file: %v", err)
+	}
+	defer file.Close()
+
+	// Save the image to the file in PNG format
+	err = png.Encode(file, im)
+	if err != nil {
+		return fmt.Errorf("error encoding image to PNG: %v", err)
+	}
+
+	fmt.Printf("Image successfully saved to %s\n", fileName)
+	return nil
+}
+
+func replaceColor(img *image.RGBA, newColor color.RGBA, palette []uint8, paletteIndex uint8) {
+
+	for i := 0; i < len(img.Pix); i += 4 {
+		if img.Pix[i+3] == paletteIndex {
+			img.Pix[i] = newColor.R
+			img.Pix[i+1] = newColor.G
+			img.Pix[i+2] = newColor.B
+			img.Pix[i+3] = newColor.A
+		}
+	}
 }
